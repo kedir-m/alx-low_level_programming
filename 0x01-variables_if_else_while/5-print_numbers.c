340d6598ed24c1b444b5545db4fbcbf694fbf18c
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-03 12:09:41 -0700

    5 print alphabet added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success) Always
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
