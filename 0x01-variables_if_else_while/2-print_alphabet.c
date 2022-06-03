dc494a078c48cb3ed4da5dec089b7d64f6315f32
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-03 10:56:25 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success) Always
+ */
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+  putchar(i);
+}   
+return (0);
+}
