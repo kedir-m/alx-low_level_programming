e129c0cd84be02d7c72542a4f6840b43c5ebabe8
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-07 09:38:55 -0700

    0-putchar added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: 0 (success) Always
+ */
+int main(void)
+{
+	int i, d;
+	char a[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	for (i = 0; i < 8; i++)
+	{
+		d = a[i];
+		putchar(d);
+	}
+	putchar(10);
+	return (0);
+}
