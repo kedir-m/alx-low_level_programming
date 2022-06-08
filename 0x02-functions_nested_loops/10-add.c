98cd05a367b0dd2381725caddd06cbe6b9b7123d
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-08 12:13:56 -0700

    10-add added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds to no.
+ * @a: pra ajh
+ * @b: para blaba bla erat bla
+ * Return: returns sum
+ *
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
