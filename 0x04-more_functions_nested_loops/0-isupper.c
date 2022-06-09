611b829f7e3eb0ceb9f8d5a2f60eb6ebe2c0ccec
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-09 07:44:52 -0700

    test files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * _isupper - checks for upper case
+ * @c: char to be checked
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z' )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ * main - checks function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int c;
+	c = 'G';
+
+	printf("%d \n", _isupper(c));
+	return (0);
+}
