79221b50f63353028283beaa2a9ede8d83d60c39
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-06-24 11:10:17 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints argument passed to it;
+ * @argc: size of argv array
+ * @argv: value passed by cmd
+ * Return: returns 1 Always
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
