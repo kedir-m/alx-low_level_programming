f79c2c656b7d8f36fb2d9bdf383fa1a54f9dada5
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-06-30 18:57:07 +0300

    hot fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocate b bytes
@@ -10,6 +10,7 @@
 void *malloc_checked(unsigned int b)
 {
 	int *ptr = malloc(b);
+
 	if (ptr == 0)
 	{
 		exit(98);
