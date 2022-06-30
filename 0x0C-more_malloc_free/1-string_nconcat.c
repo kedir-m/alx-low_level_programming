55ee718af021771d4bb2e45de15717b2beeff5b6
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-06-30 20:46:36 +0300

    task #1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n < _strlen(s2))
 	{
-		mem = malloc(_strlen(s1) * n* sizeof(char) + 1);
+		mem = malloc(_strlen(s1) * n * sizeof(char) + 1);
 
 	}
 	else
@@ -42,15 +42,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i  = 0; s1[i] != '\0'; i++)
 	{
-		m[i] = s1[i];
+		mem[i] = s1[i];
 	}
 
-	for (j = 0; j <=  n; j++, i++)
+	for (j = 0; j <  n && s2[j] != '\0'; j++, i++)
 	{
-		m[i] = s2[j];
+		mem[i] = s2[j];
 	}
 
-	m[i] = '\0';
+	mem[i] = '\0';
 
-	return (m);
+	return (mem);
 }
