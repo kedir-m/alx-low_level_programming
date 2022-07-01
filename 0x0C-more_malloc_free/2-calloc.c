d33206f4113d05c79d433fbc0994beb1e94398cd
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-07-01 22:05:09 +0300

    task #1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _memset - allocates memory for an array, using malloc
+ * @str: array length
+ * @b: size of each element
+ * @n: constant byte
+ * Return: pointer
+ */
+
+char *_memset(char *str, char b, unsigned int n)
+{
+	while (n--)
+	{
+	       *str++ = b;
+	}
+	return (str);
+}
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: array length
+ * @size: size of each element
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+	if (nmemb == 0 || size = 0)
+	{
+		return (NULL);
+	}
+	m = malloc(nmemb * size);
+	if (m == NULL)
+	{
+		return (NULL);
+	}
+
+	_memset(m, 0, nmemb * size);
+
+	return (m);
+}
