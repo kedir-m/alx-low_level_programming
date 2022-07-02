5474e10c41b0174d38ae1e5a9fa4fe0681f2e096
Kedir Mohamed <kedir@localhost.localdomain>
2022-07-02 19:36:34 +0000

    task #3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,8 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	ptr = malloc(sizeof(int) * (max - min) + 1);
+	ptr = malloc(sizeof(int) * (max - min + 1));
+
 	if (ptr == NULL)
 	{
 		return (NULL);
