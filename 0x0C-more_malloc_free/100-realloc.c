b5c2a1f12408cd8f5a435e9282f14231de110a6a
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-07-03 08:39:53 +0000

    task #100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,10 +38,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 		return (NULL);
 	}
-	else if (ptr == NULL)
+	else if (!ptr)
 	{
-		newptr = malloc(new_size);
-		return (newptr);
+		return (malloc(new_size));
 	}
 
 	else if (new_size <= old_size)
