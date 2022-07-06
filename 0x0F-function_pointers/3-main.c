af6271fe98ef3122b4a83bdeafad8817e7e5c5e6
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-07-06 18:30:04 +0000

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - checks code
+ * @argc: size of argv
+ * @argv: array
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	op = argv[2];
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(op) ==  NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && b == 0) || (*op == '/' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(a, b));
+	return 0;
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: returns int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: returns int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: returns int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: returns int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: returns int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
