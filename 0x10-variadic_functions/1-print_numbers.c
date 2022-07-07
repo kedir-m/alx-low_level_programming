5d8008eddbe2851dc47abec2853c7f5a84b9dbd4
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-07-07 19:00:55 +0000

    1-print_numbers.c added

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	va_start(args, n);)
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		int num = va_arg(args, int);
