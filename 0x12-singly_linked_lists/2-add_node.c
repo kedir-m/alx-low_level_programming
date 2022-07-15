1414aeeb302fdaa80e6418ee6688c2e4117fb622
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-15 01:12:52 -0700

    task 2 added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * _str_len - computes string length
+ * @str: string
+ * Return: returns string length
+ */
+unsigned int _str_len(const char *str)
+{
+	unsigned int ln;
+
+	for (ln = 0; str[ln]; ln++)
+		;
+
+	return (ln);
+}
+
+/**
+ * add_node - adds node in the begining
+ * @head: head node
+ * @str: string
+ * Return: returns pointer to new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *point;
+	point = malloc(sizeof(list_t));
+	if (point == NULL)
+		return (NULL);
+	point->str = strdup(str);
+	point->len = _str_len(str);
+	point->next = *head;
+	*head = point;
+
+	print_list(*head);
+
+	return (*head);
+}
