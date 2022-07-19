d33db9d1f50d2504abcaebcd01518dd8d5a5d68e
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-19 01:26:01 -0700

    hot-fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,28 @@
 #include "lists.h"
+
+/**
+ * listint_len - computes node length
+ * @h: head pointer
+ * Return: returns number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *head;
+	size_t count_node = 0;
+
+	head = h;
+
+	while (head != NULL)
+	{
+		count_node++;
+
+		head = head->next;
+	}
+
+	return (count_node);
+}
+
 /**
  * get_nodeint_at_index - gets node at index
  * @head: pointer to head node
@@ -9,7 +33,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *point;
 	unsigned int i = 0;
+	size_t nod_num;
 
+	nod_num = listint_len(head);
+	if ( index < 0 || index > nod_num)
+		return (NULL);
 	point = head;
 	while (point != NULL)
 	{
