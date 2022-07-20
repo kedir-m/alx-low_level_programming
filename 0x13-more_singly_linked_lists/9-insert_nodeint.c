94aae5169d161ba7a3d373e771f30f1b11548606
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-20 13:44:25 -0700

    fixed 9-insert...

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -47,7 +47,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (length == 0 ||idx2 == length)
 		return (add_nodeint_end(&point, n));
 
-	if (idx2 < 0 && idx2 > length)
+	if (idx2 < 0 ||  idx2 > length)
 		return (NULL);
 
 	while (point != NULL)
