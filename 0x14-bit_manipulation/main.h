51d6d1cf1b3ea5e6df883fddaca5262831a1501e
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-21 08:57:03 -0700

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@ typedef unsigned int siz_t;
 int _strlen(const char *str);
 int powr(int base, int exponent);
 siz_t binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 
 #endif
