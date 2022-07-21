abd380218d8b4a17540bab2f65140bd49c244b9b
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-21 12:26:40 -0700

    2-getbinary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int powr(int base, int exponent);
 siz_t binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
