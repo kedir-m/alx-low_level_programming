8ba53fb6c8669bf6b2d95cba7b63ccbdb31c427c
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-21 12:18:58 -0700

    fixed task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,51 +1,12 @@
 #include "main.h"
 /**
- * _pow_recursion - Search a string for any of a set of bytes.
- * @x: base
- * @y: exposant
- * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- * or NULL if no such byte is found.
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
  */
-
-unsigned long int _pow_recursion(int x, int y)
-{
-
-	if (y < 0)
-		return (-1);
-	else if (y == 1)
-		return (x);
-	else if (y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
-
-}
-
-
-/**
- * print_binary - prints decimal to binary
- * Description: first, finds the biggest exponent for 2, then go down
- * to find the smaller values
- * @n: decimal number
- */
-
 void print_binary(unsigned long int n)
 {
-	int i;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 0; _pow_recursion(2, i) <= n; i++)
-	;
-	if (n == 0)
-		i++;
-
-	for (i--; i >= 0; i--)
-	{
-		if (_pow_recursion(2, i) <= n)
-		{
-			_putchar('1');
-			n -= _pow_recursion(2, i);
-		}
-		else
-			_putchar('0');
-	}
+	_putchar((n & 1) + '0');
 }
