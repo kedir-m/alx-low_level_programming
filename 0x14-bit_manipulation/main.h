3b50128c9f8a8ecc41f1a6418f08d5cfba3e4e13
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-21 12:54:22 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ siz_t binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
