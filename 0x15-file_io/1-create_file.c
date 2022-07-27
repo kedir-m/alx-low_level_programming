3692a5e6751d9caf7feb56adab2b4bc8d9925a3b
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-07-27 03:13:20 -0700

    clfole creator added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,14 +25,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 00600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
-	{
-		fd = open(filename, O_WRONLY);
-		if (fd == -1)
-			return (-1);
-	}
+		return (-1);
 	if (text_content == NULL)
 	{
 		if (close(fd) < 0)
