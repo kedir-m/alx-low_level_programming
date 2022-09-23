d77c075b89d694ba06a6ed4ff61c38ad5ef86529
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-23 19:14:10 +0300

    fixed task#1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,36 @@
 #include "hash_tables.h"
 /**
+ * **alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @size: size of array of pointers
+ *
+ * Return: 2d array
+ */
+
+hash_table_t **alloc_grid(unsigned long int size)
+{
+	int i, j;
+	hash_table_t **tab;
+	tab = malloc(sizeof(*tab) * size);
+
+
+	for (i = 0; i < size; i++)
+	{
+		tab[i] = malloc(sizeof(**tab) * size);
+		if (tab[i] == 0)
+		{
+			/*Free everything if malloc fails*/
+			while (i--)
+				free(tab[i]);
+			free(tab);
+			return (NULL);
+		}
+	}
+
+	return (tab)
+}
+
+return (tab);
+/**
  * hash_table_create -creates hash table.
  * @size: size of hash table.
  * Return: returns pointer to newly created hash table.
@@ -10,10 +41,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table = malloc(sizeof(hash_table_t));
 
-	if (hash_table == NULL)
+	if (hash_table == NULL || size == 0)
 		return NULL;
 	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	hash_table->array = alloc_grid(size);
 
 	if (hash_table_t == NULL)
 		return NULL;
