965ccb1903a11a046c86deac9a758139f55a0cb2
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-24 10:48:30 +0300

    fixed task#1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 
+	if (size == 0)
+		return (NULL)
+
 	hash_table = malloc(sizeof(hash_table_t));
 
-	if (hash_table == NULL || size == 0)
-		return NULL;
+	if (hash_table == NULL)
+		return (NULL);
 	hash_table->size = size;
-	hash_table->array = calloc(size,  sizeof(hash_node_t));
+	hash_table->array = calloc(size, sizeof(hash_node_t));
 
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return NULL;
+		return (NULL);
 	}
 	return (hash_table);
 }
