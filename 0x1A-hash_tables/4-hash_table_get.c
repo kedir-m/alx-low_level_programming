310a4e908d5a650739beec1189ae1514c76d6d79
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-26 00:30:27 +0300

    added task4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,9 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	index = key_index((const unsigned char *)key, ht->size);
 	if (*key == '\0' || ht == NULL)
 		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	if (node == NULL)
 		return (NULL);
