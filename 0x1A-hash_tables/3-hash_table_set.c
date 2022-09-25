760355c8b3a78e1fe118f62171bcd0b57e27f3c8
Kedir Mohamed <kedir10mohamed45@gmail.com>
2022-09-25 15:02:45 +0300

    added 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(key, node->key) == 0)
 		{
 			free(node->value);
-			node->value =strdup((value);
+			node->value =strdup(value);
 			return (1);
 		}
 		node = node->next;
